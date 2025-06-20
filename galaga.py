1343277dcd22dab601b3ee6dfea4f4baf881ab01
Gino Grandin <ggrandin@csu.fullerton.edu>
2025-06-20 00:00:00 -0700

    Made it run the game scene

--- galaga.py
+++ galaga.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+from videogame.game import Galaga
+import sys
+
 def main():
-    print('hello world.')
+    sys.exit(Galaga().run())
 
 if __name__ == '__main__':
     main()
