b9e5508a9f0fdb1adc6dabe21719f28fb50d8790
Gino Grandin <ggrandin@csu.fullerton.edu>
2025-06-20 00:01:16 -0700

    Added assests being used and future used assests

--- videogame/galaga_asset_dict.py
+++ videogame/galaga_asset_dict.py
@@ -1,5 +1,13 @@
 """Asset dictionary for this game."""
 
 galaga_asset_dict = {
-
+    'playership': 'playership.png',
+    'enemyship1': 'enemyship.png',
+    'enemyship2': 'enemyship2.png',
+    'enemyship3': 'enemyship3.png',
+    'background': 'background.png',
+    'cannon ball': 'cannon ball.png',
+    'explosion': 'explosion.png',
+    'galaga_logo': 'galaga_logo.png',
+    'galaga_soundtrack': 'galaga_soundtrack.mp3'
 }
