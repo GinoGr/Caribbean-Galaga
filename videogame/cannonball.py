7d4ff66d1e6fb76c1499b4c40c50e15048ae7ade
Gino Grandin <ggrandin@csu.fullerton.edu>
2025-06-20 03:35:20 -0700

    Made cannonballs circle surfaces instead of rectangle

--- videogame/cannonball.py
+++ videogame/cannonball.py
@@ -1,109 +1,103 @@
-"""playership"""
+"""Cannonball creation here"""
 
 import pygame
 from videogame import assets
 from videogame import rgbcolors
 
-class RectSurface(pygame.Surface):
+class CannonBallSurface(pygame.Surface):
     def __init__(
-     self, width, height, color, background_color = rgbcolors.white, name = "None"       
+     self, radius, color, background_color = rgbcolors.black, name = "None"       
     ):
-        super().__init__((width, height))
+        width = 2 * radius
+        super().__init__((width, width))
+        center = (radius, radius)
         self._color = color
         self._name = name
+        self._radius = radius
         self.fill(background_color)
         #uncomment if surfaces with same color will overlap
         #self.set_colorkey(background_color)
-        self._rect_width = width
-        self._rect_height = height
-        rectangle = pygame.Rect(0, 0, self._rect_width, self._rect_height)
-        pygame.draw.rect(self, self._color, rectangle)
+
+        pygame.draw.circle(self, self._color, center, radius)
 
     @property
-    def height(self):
-        return self._rect_height
-
-    @property
-    def width(self):
-        return self._rect_width
-
+    def radius(self):
+        return self._radius
+    
     @property
     def rect(self):
         return self.get_rect()
     
 class CannonBallSprite(pygame.sprite.Sprite):
     def __init__(
-            self, position, direction, speed, width, height, 
+            self, position, direction, speed, radius, 
             color, name = "None", image_path = 'cannon ball'
     ):
         super().__init__()
-        self._ship_image = RectSurface(width = width, height = height, color = color, name = name)
+        self._cannon_ball_image = CannonBallSurface(radius, color, rgbcolors.black, name)
         self._png_image = pygame.image.load(assets.get(image_path)).convert_alpha()
-        self.image = self._png_image
-        self._start_position = (position.x, position.y)
+        self._scaled_image = pygame.transform.smoothscale(self._png_image, (25, 25))
+        self.image = self._scaled_image
+        self._start_position = position
         self.rect = self.image.get_rect()
         self.rect.center = (position.x, position.y)
         self._direction = direction
         self._speed = speed
-        self._width = width
-        self._height = height
+        self._radius = radius
         self._color = color
+        self._name = name
 
     @property
-    def height(self):
-        return self._height
-
-    @property
-    def width(self):
-        return self._width
-
+    def radius(self):
+        return self._radius
+    
     @property
     def position(self):
         return pygame.math.Vector2(self.rect.center)
-
+    
     @position.setter
     def position(self, new_position):
         if not isinstance(new_position, pygame.math.Vector2):
             raise TypeError("new_position doesn't match self._position")
-        self.rect.center = new_position
-
+        self.rect.center = (new_position.x, new_position.y)
 
     @property
     def direction(self):
         return self._direction
-
+    
     @direction.setter
     def direction(self, new_direction):
-        if not isinstance(new_direction, type(self._direction)):
+        if not isinstance(new_direction, pygame.math.Vector2):
             raise TypeError("new_direction doesn't match self._direction")
         self._direction = new_direction
 
     @property
     def speed(self):
         return self._speed
-
+    
     @speed.setter
     def speed(self, new_speed):
+        if not isinstance(new_speed, type(self._speed)):
+            raise TypeError("new_speed does not match the type of self._speed")
         self._speed = new_speed
-
+    
     @property
     def velocity(self):
         return self._direction * self._speed
-
+    
     @velocity.setter
     def velocity(self, new_velocity):
         if not isinstance(new_velocity, pygame.math.Vector2):
             raise TypeError("new_velocity doesn't match self.velocity()")
         self._speed = new_velocity.length()
-        self._direction = new_velocity
-
+        self._direction = new_velocity.normalize()
+    
     def move_ip(self, x, y):
-        """Move paddle in place"""
-        self.position += pygame.math.Vector2(x, y)
+        self.position = self.position + pygame.math.Vector2(x, y)
 
     def __repr__(self):
-        """Cannon Ball sprite in string form."""
-        stringify = f"PaddleSrite({repr(self.position)}, "
-        f"{repr(self.speed)}, {repr(self.width)}, {repr(self.height)}, "
+        """CannonBallSprite stringify."""
+        stringify = f'CannonBallSprite({repr(self.position)}, ' 
+        f'{repr(self.speed)}, {repr(self.radius)}, ' 
         f'{repr(self._color)}, "{repr(self.position)}")'
         return stringify
