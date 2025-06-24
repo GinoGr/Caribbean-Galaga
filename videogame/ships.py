d74325934e4056b66019d346bf5e040c4160b9e2
Gino Grandin <ggrandin@csu.fullerton.edu>
2025-06-24 00:34:19 -0700

    added function that creates a rushing path

--- videogame/ships.py
+++ videogame/ships.py
@@ -33,7 +33,7 @@ class RectSurface(pygame.Surface):
         return self.get_rect()
     
 class ShipSprite(pygame.sprite.Sprite):
-    def __init__(self, position, direction, speed, width, height, name = "None", image_path = 'playership', rotate_angle = False, scale_factor = 1, path = None):
+    def __init__(self, position, direction, speed, width, height, name = "None", image_path = 'playership', rotate_angle = False, scale_factor = 1, path = None, grid = None):
         super().__init__()
         #self._ship_image = RectSurface(width = width, height = height, name = name)
         self._png_image = pygame.image.load(assets.get(image_path)).convert_alpha()
@@ -52,56 +52,105 @@ class ShipSprite(pygame.sprite.Sprite):
         self._name = name
         self._initial_time = pygame.time.get_ticks()
         self._last_fire_time = 0
+        self._last_time_rush = 0
         self._is_exploading = False
         self._expload_start_time = 0
         self._state = "Idle"
-        self._base_path = path
+        if path:
+            self._base_path = list(path)
 
-        self._fire_every = randint(5000, 15000)  # Fire every 5-15 seconds
-
+        self._fire_every = randint(5000, 60000)  # Fire every 5-60 seconds
+        self._rush_every = randint(5000, 45000)
+        self.grid_spot = grid
         # Set the hitbox for the ship.
         self.hitbox = pygame.Rect(0, 0, int(self.rect.width * 0.2), int(self.rect.height * 0.7))
         self.hitbox.center = self.rect.center
 
-    def set_final_position(self, position):
-        """Set the final position of the ship."""
-        if not isinstance(position, type(self._base_path[0])):
-            raise TypeError("position must be same type as self._base_path[0]")
-        self._base_path[-2] = position
+    @property
+    def fire_every(self):
+        return self._fire_every
     
-    def comes_from_left(self):
+    @fire_every.setter
+    def fire_every(self):
+        self._fire_every = randint(5000, 60000)
+
+    @property
+    def rush_every(self):
+        return self._rush_every
+    
+    def reverse_entry_direction(self):
         new_base_path = []
         for point in self._base_path:
             new_base_path.append(((point[0] * -1) + 800, point[1]))
         self._base_path = new_base_path
-        self._path_progress = 0.0
-        self._state = "entering"
 
     @property
     def base_path(self):
         return self._base_path
 
     def enable_entry_path(self):
+        self._base_path[-2] = (self.grid_spot)
         self._entry_path = self._base_path
-        self._path_progress = 0.0
+        self._entry_path_progress = 0.0
         self._state = "entering"
 
     def update_entry(self, delta_time):
         if self._state != "entering":
             return
         path = self._entry_path
-        segs = len(path) - 3
-        u = min(self._path_progress, 0.9999) * segs
+        segment = len(path) - 3
+        u = min(self._entry_path_progress, 0.9999) * segment
         i = int(u)
-        local = u - i
-        p0, p1, p2, p3 = path[i:i+4]
-        self.rect.center = catmull_rom(p0, p1, p2, p3, local)
+        dis = u - i
+        p0, p1, p2, p3 = path[i:i + 4]
+        self.rect.center = catmull_rom(p0, p1, p2, p3, dis)
         self.hitbox.center = self.rect.center
-        self._path_progress += delta_time * 0.3
-        if self._path_progress >= 1.0:
+        self._entry_path_progress += delta_time * 0.3
+        if self._entry_path_progress >= 1.0:
             self._state = "idle"
             self.position = pygame.math.Vector2(self.rect.center)
 
+    def enable_rushing_path(self):
+        self.rush_path = []
+        direct = 1
+        self._rushing_path_progress = 0.0
+        bottom_y = self.rect.bottom
+        center_x = self.rect.centerx
+        center_y = self.rect.centery
+        self._state = "rushing"
+        self.rush_path.append((center_x, center_y))
+        self.rush_path.append((center_x, center_y))
+        for i in range(12):
+            if bottom_y  - self.height / 2 < 800:
+                center_y += 80
+                center_x += (100 * direct)
+                bottom_y += 80
+            else:
+                center_y = -self.height / 2
+                center_x += (100 * direct)
+                self.rush_path.append((-100, 900))
+                self.rush_path.append((-100, -100))
+                bottom_y = 0
+            self.rush_path.append((center_x, center_y))
+            direct *= -1
+        self.rush_path[-2] = self.grid_spot
+
+    def update_rush(self, delta_time):
+        if self._state != "rushing":
+            return
+        path = self.rush_path
+        segment = len(path) - 3
+        u = min(self._rushing_path_progress, 0.9999) * segment
+        i = int(u)
+        dis = u - i
+        p0, p1, p2, p3 = path[i:i + 4]
+        self.rect.center = catmull_rom(p0, p1, p2, p3, dis)
+        self.hitbox.center = self.rect.center
+        self._rushing_path_progress += delta_time * 0.3
+        if self._rushing_path_progress >= 1.0:
+            self._state = "idle"
+            self.position = pygame.math.Vector2(self.rect.center)
+        
     @property
     def hitbox(self):
         """Return the hitbox of the ship."""
@@ -133,6 +182,18 @@ class ShipSprite(pygame.sprite.Sprite):
         if not isinstance(new_fire_time, int):
             raise TypeError("new_fire_time must be an integer")
         self._last_fire_time = new_fire_time
+
+    @property
+    def last_time_rush(self):
+        """Return the last fire time of the ship."""
+        return self._last_time_rush
+    
+    @last_fire_time.setter
+    def last_time_rush(self, new_time_rush):
+        """Set the last fire time of the ship."""
+        if not isinstance(new_time_rush, int):
+            raise TypeError("new_fire_time must be an integer")
+        self._last_fire_time = new_time_rush
 
     @property
     def name(self):
@@ -204,8 +265,8 @@ class ShipSprite(pygame.sprite.Sprite):
         return stringify
     
 def catmull_rom(p0, p1, p2, p3, t):
-    t2, t3 = t*t, t*t*t
+    t2, t3 = t * t, t * t * t
     return pygame.Vector2(
-        0.5 * ((2*p1[0]) + (-p0[0] + p2[0]) * t + (2*p0[0] - 5*p1[0] + 4*p2[0] - p3[0]) * t2 + (-p0[0] + 3*p1[0] - 3*p2[0] + p3[0]) * t3),
-        0.5 * ((2*p1[1]) + (-p0[1] + p2[1]) * t + (2*p0[1] - 5*p1[1] + 4*p2[1] - p3[1]) * t2 + (-p0[1] + 3*p1[1] - 3*p2[1] + p3[1]) * t3)
+        0.5 * ((2 * p1[0]) + (-p0[0] + p2[0]) * t + (2 * p0[0] - 5 * p1[0] + 4 * p2[0] - p3[0]) * t2 + (-p0[0] + 3 * p1[0] - 3 * p2[0] + p3[0]) * t3),
+        0.5 * ((2 * p1[1]) + (-p0[1] + p2[1]) * t + (2 * p0[1] - 5 * p1[1] + 4 * p2[1] - p3[1]) * t2 + (-p0[1] + 3 * p1[1] - 3 * p2[1] + p3[1]) * t3)
     )
