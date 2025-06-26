a95dde4a524ea2c57a2d97d16657640a757d5c98
Gino Grandin <ggrandin@csu.fullerton.edu>
2025-06-25 17:00:51 -0700

    Added sounds

--- videogame/galaga_asset_dict.py
+++ videogame/galaga_asset_dict.py
@@ -1,18 +1,22 @@
 """
 Asset dictionary for this game.
 All assets are free to use images/sounds 
-(No attribution insisted unless commented with attributes)
+No attribution insisted but provided
 """
 
 galaga_asset_dict = {
-    "playership": "playership.png",
-    "enemyship1": "enemyship.png",
-    "enemyship2": "enemyship2.png",
-    "enemyship3": "enemyship3.png",
-    "enemyship4": "enemyship4.png",
-    "background": "background.png",
-    "cannon ball": "cannon ball.png",
-    "explosion": "explosion.png",
-    "galaga_logo": "galaga_logo.png",
-    "galaga_soundtrack": "galaga_soundtrack.mp3",
+    "playership": "playership.png", #Bleed - http://remusprites.carbonmade.com/
+    "enemyship1": "enemyship.png", #Image by quaternius on OpenGameArt.org
+    "enemyship2": "enemyship2.png", #Image by quaternius on OpenGameArt.org
+    "enemyship3": "enemyship3.png", #Image by quaternius on OpenGameArt.org
+    "enemyship4": "enemyship4.png", #Image by quaternius on OpenGameArt.org
+    "background": "background.png", #Image by MiniModele on OpenGameArt.org
+    "cannon ball": "cannon ball.png", #Credit: Flixberry Entertainment
+    "explosion": "explosion.png", #Image by Cuzco on OpenGameArt.org
+    "galaga_soundtrack": "galaga_soundtrack.mp3", #Music by Bugs Bunny from Pixabay
+    "player_death": "Playerdeath.mp3", #not required attribution
+    "player_fire": "Playerfire.mp3", #Sound Effect by LordSonny from Pixabay
+    "stationary_enemy_fire": "Enemy_stationary_fire.mp3", #Sound Effect by Universfield from Pixabay
+    "moving_enemy_fire": "enemy_moving_fire.mp3", #Sound Effect by freesound_community from Pixabay
+    "enemy_death": "enemy_death.mp3" #Sound Effect by David Dumais from Pixabay
 }
